45ededc7833662adb0365c53226f3a4f18631575
neelanqj <neelan.joachimpillai@gmail.com>
2018-05-29 10:15:09 -0400

    Init

---
+++ RedGateExample/CompareTwoDatabasesExample.cs
@@ -0,0 +1,60 @@
+﻿// using RedGate.Shared.SQL;
+using RedGate.SQLCompare.Engine;
+using RedGate.SQLCompare.Engine.Filter;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RedGateExample
+{
+    class CompareTwoDatabasesExample
+    {
+        public void RunExample()
+        {
+            using (Database stagingDB = new Database(), productionDB = new Database())
+            {
+                ConnectionProperties sourceConnectionProperties = Program.StagingConnectionProperties;
+                ConnectionProperties targetConnectionProperties = Program.ProductionConnectionProperties;
+
+
+                Console.WriteLine("Registering database " + sourceConnectionProperties.DatabaseName);
+                try
+                {
+                    stagingDB.Register(sourceConnectionProperties, Options.Default);
+                }
+                catch (SqlException e)
+                {
+                    //e.WarnUserAboutDatabaseRegistryFailure(sourceConnectionProperties);
+                    return;
+                }
+
+
+                Console.WriteLine("Registering database " + targetConnectionProperties.DatabaseName);
+                try
+                {
+
+                    productionDB.Register(targetConnectionProperties, Options.Default);
+                }
+                catch (SqlException e)
+                {
+                    //e.WarnUserAboutDatabaseRegistryFailure(targetConnectionProperties);
+                    return;
+                }
+
+
+
+                // Compare WidgetStaging against WidgetProduction
+                Differences stagingVsProduction = stagingDB.CompareWith(productionDB, Options.Default);
+
+                // Display the results on the console
+                foreach (Difference difference in stagingVsProduction)
+                {
+                    Console.WriteLine("{0} {1} {2}", difference.Type, difference.DatabaseObjectType, difference.Name);
+                }
+            }
+        }
+    }
+}
---
+++ RedGateExample/Program.cs
@@ -0,0 +1,55 @@
+﻿using RedGate.SQLCompare.Engine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RedGateExample
+{
+    class Program
+    {
+        // To install the databases used in these examples, see the "Getting Started" application under SQL Comparison SDK in the start menu.
+        private const string StagingServerName = @"(local)";
+        private const string ProductionServerName = StagingServerName;
+        private const string StagingDatabaseName = "App_FortressDb";
+        private const string ProductionDatabaseName = "App_FortressDb";
+
+       
+        //
+        // configure Sqlusername & SqlPassword if you wish to use SQL Authentication
+        //
+                                                                                                            private const string SqlUsername = "Program";
+                                                                                                            private const string SqlPassword = "think123";
+
+
+        static void Main(string[] args)
+        {
+            CompareTwoDatabasesExample r = new CompareTwoDatabasesExample();
+
+            r.RunExample();
+        }
+
+        internal static ConnectionProperties StagingConnectionProperties
+        {
+            get
+            {
+                    return new ConnectionProperties(StagingServerName, StagingDatabaseName);
+            }
+        }
+
+
+        /// <summary>
+        /// Return the connection to Production Database
+        /// </summary>
+        internal static ConnectionProperties ProductionConnectionProperties
+        {
+            get
+            {
+                    return new ConnectionProperties(ProductionServerName, ProductionDatabaseName, SqlUsername,
+                        SqlPassword);
+            }
+        }
+
+    }
+}
